0c72c88243d026499b9313360bd52a4a1d790cf7
NitheshHS <nitheshsgowda4.ng@gmail.com>
2022-01-28 13:52:13 +0530

    Testcase 2 automated

--- src/practice/MyFirstClass.java
+++ src/practice/MyFirstClass.java
@@ -4,7 +4,7 @@ public class MyFirstClass {
 
 	public static void main(String[] args) {
 		System.out.println("share this project to github");
-
+		System.out.println("Adding one more extra line");
 	}
 
 }
---
+++ src/practice/TestCase002.java
@@ -0,0 +1,8 @@
+package practice;
+
+public class TestCase002 {
+	public static void main(String[] args) {
+		System.out.println("Testcase 2 for automation");
+	}
+
+}
